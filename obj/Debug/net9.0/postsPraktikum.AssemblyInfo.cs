65da5be6f44189747ab2778147e11ffe4d2580cc
Dijar-Düzgün Somuncuyan <kartoffelheld2@gmail.com>
2025-06-16 21:59:22 +0200

    removed two unnecessary files

--- obj/Debug/net9.0/postsPraktikum.AssemblyInfo.cs
+++ obj/Debug/net9.0/postsPraktikum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("postsPraktikum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8b88845d092abe9d8f8ec76649a9577ad5b122d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+551366fcace26193472dca7ce3aa055484b78619")]
 [assembly: System.Reflection.AssemblyProductAttribute("postsPraktikum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("postsPraktikum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
